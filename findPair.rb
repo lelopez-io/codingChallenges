c7fc21c4bfd73f9a66d3492c995ad75e5593b9d5
lelopez <ux.lelopez@gmail.com>
2018-04-08 15:00:56 -0500

    Print results if pair is/ isn't found

--- findPair.rb
+++ findPair.rb
@@ -1,6 +1,8 @@
 myArray = [10, 15, 3, 7]
 val = 17
 
+pairFound = false
+
 for i in 0..(myArray.size - 2)
   for j in (i + 1)..(myArray.size - 1)
     if myArray[i] + myArray[j] == val
@@ -8,3 +10,5 @@ for i in 0..(myArray.size - 2)
     end
   end
 end
+
+puts "Pair found? #{pairFound}"
