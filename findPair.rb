ec39f87c77e8b575394aaf928a2a0730af970498
lelopez <ux.lelopez@gmail.com>
2018-04-08 14:47:25 -0500

    first commit

---
+++ findPair.rb
@@ -0,0 +1,10 @@
+myArray = [10, 15, 3, 7]
+val = 17
+
+for i in 0..(myArray.size - 2)
+  for j in (i + 1)..(myArray.size - 1)
+    if myArray[i] + myArray[j] == val
+      puts "#{myArray[i]} + #{myArray[j]} = #{val}"
+    end
+  end
+end
